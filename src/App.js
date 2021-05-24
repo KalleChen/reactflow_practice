9cac3d2f2d542205c614d68ac17d8d1827319aa1
Kalle Chen <chen1082yo@gmail.com>
2021-05-24 19:54:18 +0800

    Add final node caculation

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import ReactFlow, {
   ReactFlowProvider,
   addEdge,
   removeElements,
-  getOutgoers
+  getOutgoers,
+  Handle,
+  isNode
 } from 'react-flow-renderer'
 
 import Sidebar from './Sidebar'
@@ -37,33 +39,72 @@ class List {
   }
 }
 
+const initialLastNodes = { depth: -1, nodes: [] }
+
+const FinalNode = ({ data }) => {
+  return (
+    <div style={{ background: 'red' }}>
+      <Handle
+        type='target'
+        position='top'
+      />
+      <div style={{ padding: '20px' }}>
+        <div>
+          {data.label}
+        </div>
+      </div>
+      <Handle
+        type='source'
+        position='bottom'
+      />
+    </div>
+  )
+}
+
+const nodeTypes = {
+  finalNode: FinalNode
+}
+
 const DnDFlow = () => {
   const reactFlowWrapper = useRef(null)
   const [reactFlowInstance, setReactFlowInstance] = useState(null)
   const [elements, setElements] = useState(initialElements)
-  const getNodes = useCallback((node, current) => {
+  const [newElements, setNewElements] = useState(initialElements)
+  const [lastNodes, setLastNodes] = useState(initialLastNodes)
+  const [nodesInTree, setNodesInTree] = useState([])
+  const getNode = useCallback((node, current, depth) => {
+    setNodesInTree(prev => ([...prev, node]))
     const nodes = getOutgoers(node, elements)
+    setLastNodes(prev => {
+      if (depth > prev.depth) {
+        return { depth: depth, nodes: [node] }
+      }
+      if (depth === prev.depth) {
+        return { ...prev, nodes: [...prev.nodes, node] }
+      }
+      return prev
+    })
     nodes.forEach((node) => {
       const newNode = new Node(node.data.label)
       current.next = [...current.next, newNode]
-      getNodes(node, newNode)
+      getNode(node, newNode, depth + 1)
     })
   }, [elements])
-  const getTree = useCallback(() => {
+  const getTree = useCallback(async () => {
+    setLastNodes(initialLastNodes)
+    setNodesInTree([])
     const newNode = new Node(elements[0].data.label)
     const tree = new List(newNode)
-    getNodes(elements[0], newNode)
+    getNode(elements[0], newNode, 0)
     console.log(JSON.parse(JSON.stringify(tree.head)))
-  }, [elements, getNodes])
-  useEffect(() => {
-    getTree()
-  }, [elements, getTree])
-  const onConnect = (params) => {
+  }, [elements, getNode])
+
+  const onConnect = async (params) => {
     setElements((els) => addEdge({ ...params, animated: true }, els))
   }
-  const onElementsRemove = (elementsToRemove) =>
+  const onElementsRemove = (elementsToRemove) => {
     setElements((els) => removeElements(elementsToRemove, els))
-
+  }
   const onLoad = (_reactFlowInstance) => {
     setReactFlowInstance(_reactFlowInstance)
   }
@@ -89,18 +130,51 @@ const DnDFlow = () => {
         position,
         data: { label: node.label }
       }
-
       setElements((es) => es.concat(newNode))
     })
   }
 
+  const onClick = async (event, element) => {
+    if (isNode(element)) {
+      if (element.id !== '1') {
+        if (nodesInTree.find(node => node.id === element.id)) {
+          if (lastNodes.nodes.find(node => node.id === element.id)) {
+            if (lastNodes.nodes.length === 1) {
+              await getTree()
+            }
+          } else {
+            await setLastNodes(prev => ({ depth: -1, nodes: [{ ...element, type: 'finalNode' }] }))
+          }
+        }
+      }
+    }
+  }
+
+  useEffect(() => {
+    getTree()
+  }, [elements, getTree])
+  useEffect(() => {
+    setNewElements(prev => {
+      let final = elements
+      if (lastNodes.nodes.length === 0 || lastNodes.nodes[0].id === '1') {
+        return final
+      }
+      lastNodes.nodes.forEach(node => {
+        final = final.filter(f => f.id !== node.id)
+        final = [...final, { ...node, type: 'finalNode' }]
+      })
+      return final
+    })
+  }, [elements, setNewElements, lastNodes.nodes])
   return (
     <div className='dndflow'>
       <ReactFlowProvider>
         <Sidebar />
         <div className='reactflow-wrapper' ref={reactFlowWrapper}>
           <ReactFlow
-            elements={elements}
+            nodeTypes={nodeTypes}
+            onElementClick={onClick}
+            elements={newElements}
             onConnect={onConnect}
             onElementsRemove={onElementsRemove}
             onLoad={onLoad}
